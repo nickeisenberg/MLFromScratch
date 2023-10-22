3ed4d4846cd2cced39b4730ddb20e0f5a298e0cc
nickeisenberg <nickeisenberg@gmail.com>
2023-10-22 10:12:31 -0700

    unet model words

--- unet/model.py
+++ unet/model.py
@@ -1,8 +1,28 @@
+"""
+The unet architecture.
+Basic Diagram 
+
+inputs             outputs
+  |                    |
+(s1, p1) --(s1)-----> d1
+  |(p1) ----b1------>  |
+
+Idea:
+* s1 is a channel increase of inputs
+* p1 is a channel increase and down sample of height and width of inputs
+* b1 is a channel increase of p1. In otherwords, b1 is a channel increase and 
+  down sample of height and width of s1
+* d1 is a reconstruction with shape s1
+* outputs is a reconstruction of inputs, in otherwords, it is a channel decrease
+  of d1.
+"""
+
+
 import torch
 import torch.nn as nn
 
 
-class ConvBlockEncoder(nn.Module):
+class ConvBlock(nn.Module):
 
     def __init__(
             self, 
@@ -51,7 +71,7 @@ class EncoderBlock(nn.Module):
 
         super().__init__()
 
-        self.convblock = ConvBlockEncoder(
+        self.convblock = ConvBlock(
                 in_channels=in_channels,
                 out_channels=out_channels,
                 )
@@ -69,13 +89,12 @@ class EncoderBlock(nn.Module):
         return x, p
     
 
-class ConvBlockDecoder(nn.Module):
+class DecoderBlock(nn.Module):
 
     def __init__(
             self, 
             in_channels: int, 
             out_channels: int, 
-            skip: torch.Tensor
             ):
         
         super().__init__()
@@ -88,29 +107,85 @@ class ConvBlockDecoder(nn.Module):
             kernel_size=2
         )
 
-        self.convblock = ConvBlockEncoder(2 * out_channels, out_channels)
+        self.convblock = ConvBlock(2 * out_channels, out_channels)
 
-        self.skip = skip
 
-    def forward(self, inputs):
+    def forward(
+            self, 
+            inputs: torch.Tensor,
+            skip: torch.Tensor
+            ):
 
         x = self.convT(inputs)
-        x = torch.cat((x, self.skip), axis=1)
+        x = torch.cat((x, skip), axis=1)
         x = self.convblock(x)
 
         return x
+
+class UNet(nn.Module):
+    
+    def __init__(
+            self,
+            ):
+
+        super().__init__()
+        self.e1 = EncoderBlock(3, 16)
+        self.b  = ConvBlock(16, 32)
+        self.d1 = DecoderBlock(32, 16)
+        self.output = ConvBlock(16, 3)
+
+    def forward(
+            self,
+            inputs: torch.Tensor,
+            ):
+
+        s1, p1 = self.e1(inputs)
+        b = self.b(p1)
+        d1 = self.d1(b, s1)
+        out = self.output(d1)
+
+        return out
 
 
 
 #--------------------------------------------------
 # Testing
 #--------------------------------------------------
-input = torch.randn(size=(16, 3, 64, 64))
-
-enc = ConvBlockEncoder(3, 16, if_pooling=True)
-output = enc(input)
-output.shape
+inputs = torch.randn(size=(16, 3, 64, 64))
 
 
-dec = ConvBlockDecoder(16, 3, input)
-dec(output).shape
+cb = ConvBlock(3, 16)
+cb_out = cb(inputs)
+cb_out.shape
+
+eb = EncoderBlock(3, 16)
+eb_out = eb(inputs)
+eb_out[0].shape
+eb_out[1].shape
+
+
+b = ConvBlock(16, 32)
+b(eb_out[1]).shape
+
+db = DecoderBlock(32, 16)
+
+db(b(eb_out[1]), eb_out[0]).shape
+
+convT = nn.ConvTranspose2d(
+    32,
+    16,
+    stride=2,
+    padding=0,
+    kernel_size=2
+)
+
+convT(b(eb_out[1])).shape
+
+eb_out[0].shape
+
+conv = ConvBlock(32, 16)
+
+conv(torch.cat(
+    (convT(b(eb_out[1])),eb_out[0]), axis=1
+)).shape
+
