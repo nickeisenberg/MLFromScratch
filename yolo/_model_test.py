458be9ae8d20c97097d6a10dc8fffe4e90d31bed
nickeisenberg <nickeisenberg@gmail.com>
2023-11-15 16:12:56 -0800

    cleaned up yolo model scrip. moved tests to its own script

---
+++ yolo/_model_test.py
@@ -0,0 +1,57 @@
+import torch
+from model import *
+
+input = torch.randn(1, 3, 256, 256)
+input.shape
+
+yoloV3 = YoloV3((416, 416), 10)
+for t in yoloV3(input):
+    print(t.shape)
+
+#--------------------------------------------------
+c1 = ConvBlock(3, 32, kernel_size=3, stride=1, padding=1)(input)
+c2 = ConvBlock(32, 64, kernel_size=3, stride=2, padding=1)(c1)
+r1 = ResBlock(64)(c2)
+c1.shape
+c2.shape
+r1.shape
+
+c3 = ConvBlock(64, 128, kernel_size=3, stride=2, padding=1)(r1)
+r2 = ResBlock(128, num_repeats=2)(c3)
+c3.shape
+r2.shape
+
+c4 = ConvBlock(128, 256, kernel_size=3, stride=2, padding=1)(r2)
+scale3 = ResBlock(256, num_repeats=8)(c4)
+c4.shape
+scale3.shape
+
+c5 = ConvBlock(256, 512, kernel_size=3, stride=2, padding=1)(scale3)
+scale2 = ResBlock(512, num_repeats=8)(c5)
+c5.shape
+scale2.shape
+
+c6 = ConvBlock(512, 1024, kernel_size=3, stride=2, padding=1)(scale2)
+scale1 = ResBlock(1024, num_repeats=4)(c6)
+c6.shape
+scale1.shape
+
+scale1_pp, scale1_p = ScalePredictionBlock(1024, 1024, 10)(scale1)
+scale1_pp.shape
+scale1_p.shape
+
+u_s1 = Concatenater(512)(scale1_pp, scale2)
+u_s1.shape
+
+scale2_pp, scale2_p = ScalePredictionBlock(768, 512, 10)(u_s1)
+scale2_pp.shape
+scale2_p.shape
+
+u_s2 = Concatenater(256)(scale2_pp, scale3)
+u_s2.shape
+
+scale3_pp, scale3_p = ScalePredictionBlock(384, 256, 10)(u_s2)
+scale3_pp.shape
+scale3_p.shape
+
+
