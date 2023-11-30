1256a6750a517ac26e709b2b5c2b5d0137c13dbf
nickeisenberg <nickeisenberg@gmail.com>
2023-11-30 12:07:44 -0800

    the decoder works and the model produced good results on the training data

--- yolo/train_model/post_train/load_weights.py
+++ yolo/train_model/post_train/load_weights.py
@@ -1,3 +1,5 @@
+from PIL import Image, ImageDraw
+from torchvision.transforms import ToPILImage
 import torch
 import torch.nn as nn
 from train_model.settings import model_inputs, num_classes, t_at
@@ -6,6 +8,9 @@ import matplotlib.pyplot as plt
 from model import YoloV3, YoloV3Loss
 from utils import scale_anchors, BuildTarget
 
+#--------------------------------------------------
+# Instantate the model and BuildTarget class
+#--------------------------------------------------
 scales = model_inputs['scales']
 anchors = model_inputs['anchors']
 cat_map = t_at.cat_mapper
@@ -13,21 +18,29 @@ cat_map = t_at.cat_mapper
 bt = BuildTarget(cat_map, anchors, scales, 640, 512)
 
 yoloV3 = YoloV3(1, scales, num_classes)
+#--------------------------------------------------
 
+#--------------------------------------------------
+# Load model weights
+#--------------------------------------------------
 modelroot = f"{os.environ['HOME']}/GitRepos/ml_arcs/yolo/train_model"
 train_pth_path = modelroot + "/state_dicts/yolo_train0.pth"
 
 yoloV3.load_state_dict(torch.load(train_pth_path))
+#--------------------------------------------------
 
-image, target = model_inputs['t_dataset'][0]
+#--------------------------------------------------
+# Get a training image and target and prediction
+#--------------------------------------------------
+image, target = model_inputs['t_dataset'][1]
 image = image.unsqueeze(0)
 target = [t.unsqueeze(0) for t in target]
-
-plt.plot(target[1][..., 3: 4].detach().reshape(-1))
-plt.show()
-
 pred = [p for p in yoloV3(image)]
+#--------------------------------------------------
 
+#--------------------------------------------------
+# See the models loss on this image
+#--------------------------------------------------
 loss_fn = YoloV3Loss("cpu")
 loss = {}
 for scale_id, (p, t) in enumerate(zip(pred, target)):
@@ -42,7 +55,45 @@ for scale_id, (p, t) in enumerate(zip(pred, target)):
         loss[k] += loss_hist[k]
 for k in loss:
     print(k, loss[k])
+#--------------------------------------------------
 
-len(bt.decode_tuple(target, .8, 1, False))
 
-len(bt.decode_tuple(pred, .75, 1, True))
+#--------------------------------------------------
+# Decode the target and the prediction and get the bounding box info
+#--------------------------------------------------
+target = [t[0] for t in target]
+pred = [p[0] for p in pred]
+image = image[0]
+
+target_bbox = bt.decode_tuple(target, .8, 1, False)
+pred_bbox = bt.decode_tuple(pred, .75, .8, True)
+#--------------------------------------------------
+
+for b in target_bbox:
+    print(b['bbox'])
+
+#--------------------------------------------------
+# Plot the bounding boxes
+#--------------------------------------------------
+
+image_pil = ToPILImage()(image)
+
+if image_pil.mode == 'L':
+    rgb_img = Image.new("RGB", image_pil.size)
+    rgb_img.paste(image_pil)
+    image_pil = rgb_img
+draw = ImageDraw.Draw(image_pil)
+for bbox in pred_bbox:
+    bbox = bbox['bbox']
+    for i in [0]:
+        bottom_right_x = bbox[0] + i + bbox[2]
+        bottom_right_y = bbox[1] + i + bbox[3]
+        draw.rectangle(
+            (bbox[0] + i, bbox[1] + i, bottom_right_x, bottom_right_y), 
+            outline ="red"
+        )
+image_pil.show()
+
+
+
+
