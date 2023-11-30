b1e0c6227b800ac4a726fa239a8acd6e52c878ba
nickeisenberg <nickeisenberg@gmail.com>
2023-11-29 23:13:42 -0800

    changed to 100 epochs

--- yolo/train_model/train_model.py
+++ yolo/train_model/train_model.py
@@ -14,7 +14,7 @@ save_best_train_to = modelroot + "/state_dicts/yolo_train.pth"
 save_best_val_to = modelroot + "/state_dicts/yolo_val.pth"
 save_train_loss_csv_to = modelroot + "/lossdfs/train.csv"
 save_val_loss_csv_to = modelroot + "/lossdfs/val.csv"
-epochs = 50
+epochs = 100
 
 yoloV3model.fit(
     num_epochs=epochs, 
