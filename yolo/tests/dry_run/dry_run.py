994e83a42a689e236d1643f58258201b8b0de707
nickeisenberg <nickeisenberg@gmail.com>
2023-11-30 16:14:10 -0800

    did a dry run

---
+++ yolo/tests/dry_run/dry_run.py
@@ -0,0 +1,20 @@
+from tests.dry_run.dry_run_settings import model_inputs
+from model import Model
+import os
+
+yoloV3model = Model(**model_inputs)
+
+modelroot = f"{os.environ['HOME']}/GitRepos/ml_arcs/yolo/tests/dry_run"
+save_best_train_to = modelroot + "/state_dicts/yolo_train.pth"
+save_best_val_to = modelroot + "/state_dicts/yolo_val.pth"
+save_train_loss_csv_to = modelroot + "/lossdfs/train.csv"
+save_val_loss_csv_to = modelroot + "/lossdfs/val.csv"
+epochs = 2
+
+yoloV3model.fit(
+    num_epochs=epochs, 
+    save_best_train_model_to=save_best_train_to,
+    save_best_val_model_to=save_best_val_to,
+    save_train_loss_csv_to=save_train_loss_csv_to,
+    save_val_loss_csv_to=save_val_loss_csv_to,
+)
--- yolo/tests/dry_run/dry_run_eda.py
+++ yolo/tests/dry_run/dry_run_eda.py
@@ -1,4 +1,4 @@
-from tests.pre_train.dry_run_settings import model_inputs
+from tests.dry_run.dry_run_settings import model_inputs
 from model import Model
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,18 +11,8 @@ yoloV3model = Model(**model_inputs)
 
 modelroot = f"{os.environ['HOME']}/GitRepos/ml_arcs/yolo/tests/pre_train"
 save_best_train_to = modelroot + "/state_dicts/yolo_train.pth"
-save_best_val_to = modelroot + "/state_dicts/yolo_val.pth"
-save_train_loss_csv_to = modelroot + "/lossdfs/train.csv"
-save_val_loss_csv_to = modelroot + "/lossdfs/val.csv"
-epochs = 2
 
-yoloV3model.fit(
-    num_epochs=epochs, 
-    save_best_train_model_to=save_best_train_to,
-    save_best_val_model_to=save_best_val_to,
-    save_train_loss_csv_to=save_train_loss_csv_to,
-    save_val_loss_csv_to=save_val_loss_csv_to,
-)
+yoloV3model.load_state_dict(save_best_train_to)
 
 img, tar = model_inputs["t_dataset"][0]
 
