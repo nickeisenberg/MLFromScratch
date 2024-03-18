7a268dcbca8991e2224f4b075b288ef3b5dcd6b9
nickeisenberg <nickeisenberg@gmail.com>
2024-03-18 11:55:45 -0700

    SelfAttention layer

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class SelfAttention(nn.Module):
     def __init__(self, k, heads=4, mask=False) -> None:
@@ -11,7 +12,27 @@ class SelfAttention(nn.Module):
         self.tokeys = nn.Linear(k, k, bias=False)
         self.toqueries = nn.Linear(k, k, bias=False)
         self.tovalues = nn.Linear(k, k, bias=False)
-        self.unifyheads = nn.Linear(k, k, bias=False)
+        self.unifyheads = nn.Linear(k, k)
 
+    def forward(self, x: torch.Tensor):
+        b, t, k = x.size()
+        s = k // self.heads
 
+        queries = self.toqueries(x)
+        keys = self.tokeys(x)
+        values = self.tovalues(x)
 
+        keys = keys.transpose(1, 2).contiguous().view(b * self.heads, t, s)
+        queries = queries.transpose(1, 2).contiguous().view(b * self.heads, t, s)
+        values = values.transpose(1, 2).contiguous().view(b * self.heads, t, s)
+
+        dot = torch.bmm(queries, keys.transpose(1, 2))
+        dot = dot / (k ** (1/2))
+        dot = F.softmax(dot, dim=2)
+        out = torch.bmm(dot, values).view(b, self.heads, t, s)
+        out = out.transpose(1, 2).contiguous().view(b, t, s * self.heads)
+
+        return self.unifyheads(out)
+
+self_attention = SelfAttention(12)
+self_attention(torch.randn((10, 15, 12))).shape
