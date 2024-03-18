641e468272f4a5761a3f8f3c6fe1f4ccf3d87792
nickeisenberg <nickeisenberg@gmail.com>
2024-03-18 13:06:20 -0700

    basic transformer

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -1,4 +1,3 @@
-from collections.abc import Callable
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -58,8 +57,40 @@ class TransformerBlock(nn.Module):
         return self.norm2(fedforward + x)
 
 
-self_attention = SelfAttention(12)
-self_attention(torch.randn((10, 15, 12))).shape
+class Transformer(nn.Module):
+    def __init__(self, k, heads, depth, seq_length, num_tokens, num_classes):
+        super().__init__()
 
-transformer_block = TransformerBlock(12, 4)
-transformer_block(torch.randn((10, 15, 12))).shape
+        self.num_tokens = num_tokens
+        self.token_emb = nn.Embedding(num_tokens, k)
+        self.pos_emb = nn.Embedding(seq_length, k)
+
+        tblocks = []
+        for _ in range(depth):
+            tblocks.append(TransformerBlock(k=k, heads=heads))
+        self.tblocks = nn.Sequential(*tblocks)
+
+        self.toprobs = nn.Linear(k, num_classes)
+
+    def forward(self, x):
+        """
+        :param x: A (b, t) tensor of integer values representing
+                  words (in some predetermined vocabulary).
+        :return: A (b, c) tensor of log-probabilities over the
+                 classes (where c is the nr. of classes).
+        """
+        tokens = self.token_emb(x)
+        b, t, k = tokens.size()
+        
+        positions = torch.arange(t)
+        positions = self.pos_emb(positions).unsqueeze(0).expand(b, t, k)
+        
+        x = tokens + positions
+        x = self.tblocks(x)
+        
+        x = self.toprobs(x.mean(dim=1))
+        return F.log_softmax(x, dim=1)
+
+
+transformer = Transformer(12, 4, 2, 25, 400, 5)
+transformer(torch.randint(0, 400, (33, 25))).shape
